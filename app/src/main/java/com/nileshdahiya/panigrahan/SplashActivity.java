93368864f67c19aea0aec5afc2c50485966a1383
chhotuthakur <npsdahiya@gmail.com>
2023-12-18 00:20:50 +0530

    update 3rd

--- app/src/main/java/com/nileshdahiya/panigrahan/SplashActivity.java
+++ app/src/main/java/com/nileshdahiya/panigrahan/SplashActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -26,13 +27,20 @@ public class SplashActivity extends AppCompatActivity {
         final Animation slideUpAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_up);
 
         // Set up a listener to start the animation when the logo is clicked
-        logoImageView.setOnClickListener(new View.OnClickListener() {
+//        logoImageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                fragmentContainer.setVisibility(View.VISIBLE);
+//                fragmentContainer.startAnimation(slideUpAnimation);
+//            }
+//        });
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
+            public void run() {
                 fragmentContainer.setVisibility(View.VISIBLE);
                 fragmentContainer.startAnimation(slideUpAnimation);
             }
-        });
+        },4000);
 
         // Load the login fragment
         LoginFragment loginFragment = new LoginFragment();
